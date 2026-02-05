909db135563c4605ac1dbacbe77c48f982c931f7
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2026-02-05 18:21:32 -0300

    feat: adição de recuperação de dados

--- src/components/tabela.jsx
+++ src/components/tabela.jsx
@@ -1,7 +1,24 @@
 import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, Paper } from '@mui/material';
 import CoreTableContainer from '../Core/CoreTableContainer';
 import CoreTableHead from '../Core/CoreTableHead';
+import { useSignals, useSignal } from '@preact/signals-react/runtime';
+import axios from 'axios';
+import { useEffect } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
 function Tabela() {
+	useSignals();
+	const apiUrl = import.meta.env.VITE_API_URL;
+	const dadosPacientes = useSignal([]);
+	const recuperarDados = async () => {
+		const response = await axios.get(`${apiUrl}/registros`);
+
+		dadosPacientes.value = response.data.resposta;
+		console.log('dadosPacientes.value', dadosPacientes.value);
+	};
+
+	useEffect(() => {
+		recuperarDados();
+	}, []);
 	return (
 		<CoreTableContainer component={Paper}>
 			<Table>
@@ -14,12 +31,22 @@ function Tabela() {
 					</TableRow>
 				</CoreTableHead>
 				<TableBody>
-					<TableRow>
-						<TableCell>paciente 1</TableCell>
-						<TableCell>saida 1</TableCell>
-						<TableCell>data 1</TableCell>
-						<TableCell>Editar</TableCell>
-					</TableRow>
+					{dadosPacientes.value.length > 0 ? (
+						dadosPacientes.value.map((paciente, index) => (
+							<TableRow>
+								<TableCell>{paciente.nome_paciente}</TableCell>
+								<TableCell>{paciente.data_entrada}</TableCell>
+								<TableCell>{paciente.saida_exame}</TableCell>
+								<TableCell>
+									<EditIcon />
+								</TableCell>
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell>Sem dados disponiveis</TableCell>
+						</TableRow>
+					)}
 				</TableBody>
 			</Table>
 		</CoreTableContainer>
